5eec0d947c78471a8e868ead50eda6a99387a75b
kirillmisnik <kirill.misnik.g@gmail.com>
2021-08-23 12:29:29 +0300

    Added security configuration

---
+++ src/main/java/edu/netcracker/messenger/security/SecurityConfiguration.java
@@ -0,0 +1,25 @@
+package edu.netcracker.messenger.security;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+                .csrf().disable();
+        http
+                .formLogin().permitAll();
+        http
+                .authorizeRequests()
+                .antMatchers("/registration").permitAll();
+        http
+                .authorizeRequests()
+                .anyRequest().authenticated();
+    }
+}
