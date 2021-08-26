715f1612d8f0f426032bdf1400f2bdedf551c4be
kirillmisnik <kirill.misnik.g@gmail.com>
2021-08-26 13:54:51 +0300

    Added user controllers and exception handlers

--- src/main/java/edu/netcracker/messenger/model/user/UserRepository.java
+++ src/main/java/edu/netcracker/messenger/model/user/UserRepository.java
@@ -1,6 +1,7 @@
 package edu.netcracker.messenger.model.user;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 
 public interface UserRepository extends JpaRepository<User, Long>  {
@@ -14,4 +15,11 @@ public interface UserRepository extends JpaRepository<User, Long>  {
     @Query(value = "SELECT * FROM users WHERE email = ?1", nativeQuery = true)
     User getByEmail(String email);
 
+    @Modifying
+    @Query(value = "DELETE FROM chat_members WHERE user_id = ?1", nativeQuery = true)
+    void deleteChatMemberById(Long id);
+
+    @Modifying
+    @Query(value = "DELETE FROM messages WHERE user_id = ?1", nativeQuery = true)
+    void deleteMessagesById(Long id);
 }
