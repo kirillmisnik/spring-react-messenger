daa0a53fbe5fa0a9c398a5af1a1209675912b52b
kirillmisnik <kirill.misnik.g@gmail.com>
2021-08-26 15:18:05 +0300

    Added chat model and repository

---
+++ src/main/java/edu/netcracker/messenger/model/chat/ChatRepository.java
@@ -0,0 +1,5 @@
+package edu.netcracker.messenger.model.chat;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ChatRepository extends JpaRepository<Chat, Long> {}
---
+++ src/main/java/edu/netcracker/messenger/model/chat/ChatType.java
@@ -0,0 +1,6 @@
+package edu.netcracker.messenger.model.chat;
+
+public enum ChatType {
+    GROUP,
+    PERSONAL
+}
